789db012995a6d2813a71864cceb4e79ebb9f249
hades42 <nguyenvannguyen.oc@gmail.com>
2021-04-05 12:28:03 +1000

    Fixing the animation for the modal

--- src/App.js
+++ src/App.js
@@ -47,12 +47,14 @@ class App extends Component {
             ></div>
           )}
         </Transition>
-
+              {/* Transition for the modal */}
+        <Transition mountOnEnter unmountOnExit in={this.state.modalIsOpen} timeout={300}>
+          {(state) => (
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
         {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
+          <Backdrop show />
         ) : null}
         <button onClick={this.showModal} className="Button">
           Open Modal
